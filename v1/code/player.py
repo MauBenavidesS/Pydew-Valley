c5d1316720931373bfce06468ed95de9f17c7bd4
MauBenavidesS <maubs214@gmail.com>
2023-06-28 11:38:15 +0900

    Added Player direction vector change

--- v1/code/player.py
+++ v1/code/player.py
@@ -5,21 +5,32 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        # general setup
         self.image = pygame.Surface((32, 64))
         self.image.fill('green')
         self.rect = self.image.get_rect(center = pos)
+
+        # movement attributes
+        self.direction = pygame.math.Vector2()
     
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-            print('up')
+            self.direction.y = -1
         elif keys[pygame.K_DOWN]:
-            print('down')
-        elif keys[pygame.K_RIGHT]:
-            print('right')
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
         elif keys[pygame.K_LEFT]:
-            print('left')
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+        
+        print(self.direction)
 
     def update(self, dt):
         self.input()
